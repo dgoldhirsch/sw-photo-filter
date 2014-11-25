22a97ff2c961c247d5b895c8b2015d8263360589
David Charles Goldhirsch <dgoldhirsch@yahoo.com>
2014-11-25 07:34:36 -0500

    Copied from Apple Tutorial

--- sw-photo-filter/ViewController.swift
+++ sw-photo-filter/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pictureView: UIImageView!
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        // Create image to filter
+        let inputImage = CIImage(image: pictureView.image)
+        
+        // Create random color
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply filter
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        // Update UI
+        pictureView.image = UIImage(CGImage: renderedImage)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
